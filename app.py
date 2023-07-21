c4d3a004b48bc08208f3896441e2e01b18687464
Shadrack <shabawak@gmail.com>
2023-07-21 12:31:29 +0000

    User-authentication

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, request, render_template, url_for
+from flask import Flask, request, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
+
 import numpy as np
 import pickle
 
@@ -10,21 +15,50 @@ app = Flask(__name__)
 
 #connecting to the database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SECRETE_KEY'] = 'thisisasecretekey'
+app.config['SECRET_KEY'] = 'thisisasecretekey'
 #initializing the database
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 #Creating the database table
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False)
+    username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
 
 # Add the application context to perform database operations
 with app.app_context():
     # Perform database operations within the application context
     db.create_all()
- 
+
+# Handles registration
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder":"username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder":"password"})
+    submit = SubmitField("Register")
+
+    def validate_username(self, username):
+        existing_user_name = User.query.filter_by(username=username.data).first()
+        if existing_user_name:
+            registration_error ="This username already exist, choose another"
+            print(registration_error)
+            raise ValidationError(registration_error)
+    
+
+# Handles login
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder":"username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder":"password"})
+    submit = SubmitField("Login")
 
 #load pickle model
 model = pickle.load(open("model.pkl", "rb"))
@@ -35,14 +69,45 @@ def home():
     return render_template("home.html")
 
 #Route to the login page
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    form = LoginForm()
 
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('dashboard'))
+
+    return render_template("login.html", form=form)
+
+# Route to dashboard
+@app.route('/dashboard', methods=['GET', 'POST'])
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
+
+
+# Route to handle logout
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 #Route to the register page
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template("register.html")
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+
+    return render_template("register.html", form=form)
 
 #Route to the ML page
 @app.route("/predict", methods=["POST"])
@@ -55,7 +120,7 @@ def predict():
 
     return render_template("index.html", prediction_value = "The portability value is {}".format(prediction))
 
-
+#Main 
 if __name__ == "__main__":
     app.run(debug=True)
 
